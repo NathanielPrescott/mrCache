04e8ea83625c329c7bed48f331d1ded353ea58fe
Nathan Prescott <echodog@gmail.com>
2023-11-30 10:29:37 -0800

    feature-1: updated main function to handle the proto generated server and tested via postman

--- src/main.rs
+++ src/main.rs
@@ -21,24 +21,29 @@ struct RedisCacheService {
 #[tonic::async_trait]
 impl RedisCache for RedisCacheService {
     async fn get(&self, request: Request<Key>) -> Result<Response<Value>, Status> {
-        println!("Got a request: {:?}", request);
-
-        Ok(Response::new(Default::default()))
+        Ok(Response::new(Value {
+            value: "Get gRPC call temp value".to_string(),
+        }))
     }
 }
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() {
     let grpc_port = "50051";
 
     println!("Starting server...");
     println!("gRPC listening on: http://localhost:{}", grpc_port);
 
-    // let service = RedisCacheServer::new();
+    let service = RedisCacheServer::new(RedisCacheService {
+        redis: Box::leak(Box::new(Cache {
+            key: "key".to_string(),
+            value: "value".to_string(),
+        })),
+    });
     let address = "[::1]:".to_owned() + grpc_port;
 
     Server::builder()
-        .add_service(/* add Service here */)
+        .add_service(service)
         .serve(address.parse().unwrap())
         .await
         .unwrap()
